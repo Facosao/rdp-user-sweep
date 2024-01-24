3bfc6e98d885b44462d101f525885672d11e54de
Facosao <leonardosalgado02@hotmail.com>
2024-01-24 18:03:37 -0300

    gen_hosts

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,42 @@ use std::{
     process::Command,
 };
 
+trait IPV4as32bit {
+    fn from_u32(address: u32) -> Ipv4Addr;
+    fn to_u32(address: Ipv4Addr) -> u32;
+}
+
+impl IPV4as32bit for Ipv4Addr {
+    fn from_u32(address: u32) -> Ipv4Addr {
+        let mut octets: [u8; 4] = [0; 4];
+        octets[0] = (address & (255 << 24)) as u8;
+        octets[1] = (address & (255 << 16)) as u8;
+        octets[2] = (address & (255 << 8)) as u8;
+        octets[3] = (address & 255) as u8;
+
+        Ipv4Addr::new(octets[0], octets[1], octets[2], octets[3])
+    }
+
+    fn to_u32(address: Ipv4Addr) -> u32 {
+        let addr_octets = address.octets();
+        for value in addr_octets {
+            println!("value = {}", value);
+        }
+
+        let mut addr32: u32;
+
+        addr32 = addr_octets[0].rotate_left(24) as u32;
+        addr32 |= addr_octets[1].rotate_left(16) as u32;
+
+        println!("mid stage = {}", addr32);
+
+        addr32 |= addr_octets[2].wrapping_shl(8) as u32;
+        addr32 |= addr_octets[3] as u32;
+
+        addr32
+    }
+}
+
 fn ping(addr: &str) -> Result<(), AddrParseError> {
     let addr_port = format!("{}:3389", addr); // RDP port number
     
@@ -74,15 +110,27 @@ fn is_broadcast_address(address: Ipv4Addr, mask: Ipv4Addr) -> bool {
 }
 
 fn gen_hosts(address: Ipv4Addr, mask: Ipv4Addr) -> Vec<Ipv4Addr> {
-    let addr_octets = address.octets();
-    let mask_octets = mask.octets();
-    let mut hosts: Vec<Ipv4Addr> = Vec::new();
+    let mut addr_octets = address.octets();
+    
 
-    for i in 3..=0 {
-        let mut j = 0;
-        while addr_octets[i] + j <= 255 {
-                       
-        }
+    
+    
+    
+    println!("start address: {}", address);
+    println!("start mask   : {}", mask);
+
+    let mut hosts: Vec<Ipv4Addr> = Vec::new();
+    let mut addr32 = Ipv4Addr::to_u32(address);
+    let mask32 = Ipv4Addr::to_u32(mask);
+
+    println!("start addr32: {}", addr32);
+    println!("start mask32: {}", mask32);
+    //addr32 += 1;
+
+    while (addr32 | mask32) != (u32::MAX) {
+        hosts.push(Ipv4Addr::from_u32(addr32));
+        //println!("current host = {}", Ipv4Addr::from_u32(addr32));
+        addr32 += 1;
     }
 
     hosts
@@ -90,6 +138,14 @@ fn gen_hosts(address: Ipv4Addr, mask: Ipv4Addr) -> Vec<Ipv4Addr> {
 
 fn main() {
     let args: Vec<_> = std::env::args().collect();
+
+    let all_hosts = gen_hosts(Ipv4Addr::from_str(&args[1]).unwrap(), Ipv4Addr::from_str(&args[2]).unwrap());
+    for host in all_hosts {
+        println!("host = {}", host);
+    }
+
+    /*
+
     let _ = ping(&args[1]);
     let raw_user = query(&args[1]);
 
@@ -99,6 +155,8 @@ fn main() {
             println!("user: {}", item);
         }
     }
+
+    */
 
     /*
     println!(" USERNAME              SESSIONNAME        ID  STATE   IDLE TIME  LOGON TIME");
